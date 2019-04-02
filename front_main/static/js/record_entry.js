c5825b1bda8df51ee60710b105366813b468cf8b
kartik <kartikgautam1710@gmail.com>
2019-04-03 01:11:16 +0530

    halfway through

---
+++ front_main/static/js/record_entry.js
@@ -0,0 +1,7 @@
+<script src="https://code.jquery.com/jquery-3.2.1.js"> 
+
+ <script type="text/javascript">
+	 	$(".input").focus(function() {
+	 		$(this).parent().addClass("focus");
+	 	}) 
+	 </script>
