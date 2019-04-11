d7a61aaa8957d2f97d6873e45e3d5de76f5c1f0c
kartik <kartikgautam1710@gmail.com>
2019-04-11 13:06:40 +0530

    add record working

--- front_main/teacher/views.py
+++ front_main/teacher/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import manager
 # Create your views here.
+
+
 def index(request):
 	context ={
 
@@ -11,8 +13,12 @@ def index(request):
 
 
 def record_entry(request):
-	context ={}
-	
+	"""
+	render form to input data
+	"""
+	#global number,name,branch
+
+	context = {}
 
 	return render (request,'teacher/record_entry.html',context)
 
@@ -31,3 +37,38 @@ def teacher_record_check(request):
 
 
 	return render (request,'teacher/teacher_record_check.html',context)
+
+
+
+
+
+def teacher_entry(request):
+
+	""" 
+	process data from form
+	"""
+	#global number,name,branch
+	number,name,branch = None,None,None
+	
+	number = request.POST.get('Number')
+	name = request.POST.get('Name')
+	branch = request.POST.get('Branch')
+	context ={}
+
+
+
+	
+	
+	boole = manager.teach_entry(number,name,branch)
+
+
+	context = {
+	'number': number,
+	'name': name,
+	'branch' : branch,
+	'bool' : boole,
+
+	}
+
+
+	return render(request,'teacher/teacher_entry.html',context)
